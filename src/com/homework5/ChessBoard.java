43bae36c1528f0be54aab1270cfd2bc7d3e3df5f
Alexander Dulski <sasha684549@gmail.com>
2022-10-10 19:22:09 +0300

    optimization code

--- src/com/homework5/ChessBoard.java
+++ src/com/homework5/ChessBoard.java
@@ -2,14 +2,10 @@ package com.homework5;
 
 public class ChessBoard {
     public static void main(String[] args) {
-        char[][] chessBoard = new char[8][8];
+        String[][] chessBoard = new String[8][8];
         for (int i = 0; i < chessBoard.length; ++i) {
             for (int j = 0; j < chessBoard.length; ++j) {
-                if ((i + j) % 2 == 0) {
-                    System.out.print('W' + " ");
-                } else {
-                    System.out.print('B' + " ");
-                }
+                System.out.print(chessBoard[i][j] = ((i + j) % 2 == 0) ? "W " : "B ");
             }
             System.out.println();
         }
