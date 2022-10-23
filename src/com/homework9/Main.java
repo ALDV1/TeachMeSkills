9c5a26f1ee6261da4745b87ad72ff460e49ff403
Alexander Dulski <sasha684549@gmail.com>
2022-10-23 23:56:04 +0300

    some changes in homework9

--- src/com/homework9/Main.java
+++ src/com/homework9/Main.java
@@ -6,43 +6,43 @@ public class Main {
     public static void main(String[] args) {
 
         Hen[] hen = new Hen[100];
-        boolean moldovaAlreadyBeen = true;
-        boolean russiaAlreadyBeen = true;
-        boolean belarusAlreadyBeen = true;
-        boolean ukraineAlreadyBeen = true;
+        boolean moldovaAlreadyBeen = false;
+        boolean russiaAlreadyBeen = false;
+        boolean belarusAlreadyBeen = false;
+        boolean ukraineAlreadyBeen = false;
 
         for (int i = 0; i < 100; ++i) {
             int randNumber = 1 + (int) (Math.random() * 4);
             if (randNumber == 1) {
                 hen[i] = new MoldovanHen();
-                if (moldovaAlreadyBeen) {
+                if (!moldovaAlreadyBeen) {
                     System.out.println(hen[i].getDescription());
                 }
-                moldovaAlreadyBeen = false;
+                moldovaAlreadyBeen = true;
                 continue;
             }
             if (randNumber == 2) {
                 hen[i] = new RussianHen();
-                if (russiaAlreadyBeen) {
+                if (!russiaAlreadyBeen) {
                     System.out.println(hen[i].getDescription());
                 }
-                russiaAlreadyBeen = false;
+                russiaAlreadyBeen = true;
                 continue;
             }
             if (randNumber == 3) {
                 hen[i] = new BelarusianHen();
-                if (belarusAlreadyBeen) {
+                if (!belarusAlreadyBeen) {
                     System.out.println(hen[i].getDescription());
                 }
-                belarusAlreadyBeen = false;
+                belarusAlreadyBeen = true;
                 continue;
             }
             if (randNumber == 4) {
                 hen[i] = new UkrainianHen();
-                if (ukraineAlreadyBeen) {
+                if (!ukraineAlreadyBeen) {
                     System.out.println(hen[i].getDescription());
                 }
-                ukraineAlreadyBeen = false;
+                ukraineAlreadyBeen = true;
             }
         }
         System.out.println("count of eggs per year: " + Hen.eggsOfAll);
